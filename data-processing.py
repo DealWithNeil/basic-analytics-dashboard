eee419b61234a220407656fde309e358e4c50822
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-04-05 16:59:31 +0800

    fix encoding error

--- data-processing.py
+++ data-processing.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\Maranello Nacionales\analytics-dashboard\basic-analytics-dashboard\sales_data_sample.csv")
+df = pd.read_csv(r"C:\Users\Maranello Nacionales\analytics-dashboard\basic-analytics-dashboard\sales_data_sample.csv", encoding='latin-1')
 df.head()
