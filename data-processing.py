bd8cf956683ee07dea643c089784206d0e175391
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-04-05 17:01:06 +0800

    updated the code for null values and sum values

--- data-processing.py
+++ data-processing.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 df = pd.read_csv(r"C:\Users\Maranello Nacionales\analytics-dashboard\basic-analytics-dashboard\sales_data_sample.csv", encoding='latin-1')
 df.head()
+
+print(df.info())
+print(df.isnull().sum())
